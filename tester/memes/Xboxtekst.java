d279d49124ec505add626993864e75fe2d7e39b6
yllnor13 <yllruk13@gmail.com>
2020-06-04 16:49:56 +0200

    xbox one got released

--- tester/memes/Xboxtekst.java
+++ tester/memes/Xboxtekst.java
@@ -2,7 +2,7 @@ public class Xboxtekst{
     static int xbox = 2;
     public static void main(String[] args){
         System.out.println("Xbox 360? what happened to...");
-        System.out.print("Xbox 1, ");
+        System.out.print("Xbox 2, ");
         while(xbox < 359){
             xbox ++;
             System.out.print(", Xbox " + xbox);
