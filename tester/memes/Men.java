daef6367f40fe4403d52ed3a8c6d2c2266e4d665
yllnor13 <yllnor13@gmail.com>
2020-06-05 15:07:17 +0200

    memes update

---
+++ tester/memes/Men.java
@@ -0,0 +1,9 @@
+public class Men {
+    private static int men = 0;
+    public static void main(String args[]){
+        while(men <= 2020){
+            System.out.println("men aint shit " + men);
+            men ++;
+        }
+    }
+}
