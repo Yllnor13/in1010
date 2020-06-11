1b8e344e45078b3eff5f6848f0bb0ec27e06971c
yllnor13 <yllnor13@gmail.com>
2020-06-11 03:39:33 +0200

    ferdig(tror jeg)

--- Eksamen/Oppgave3/Oppgave3/Brukergrensesnitt.java
+++
@@ -1,6 +0,0 @@
-public interface Brukergrensesnitt {
-    public void giStatus(String status); //som gir brukeren informasjon om det som skjer i spillet (foreksempel om det stedet spilleren er kommet til). 
-
-    public int beOmKommando(String spoersmal, String[] alternativer); //som gir brukeren et valg (parameteren spoersmaal) med noen alternativer (parameteren alternativer). Metoden returnerer et heltall som angir indeksen for det valgte alternativet.
-
-}
